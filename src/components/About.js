59d9ef5625eea5701542502dffdbcd622d84e419
Faiz1604 <faizsheikh4131@gmail.com>
2022-08-07 14:54:20 +0530

    about page change

--- src/components/About.js
+++ src/components/About.js
@@ -33,16 +33,7 @@ export default function About(props) {
     <div className="container" style={myStyle}>
         <h1>About</h1>
     <p className="my-2">
-        Why is the About Us Page Important?
-        The About Us page is an important part of your website. It’s not just about you, it’s about your business too! It is the first one to be noticed by your visitors.
-
-        Many web designers consider creating an About Us page a challenge because there are so many things that need to be included in it.
-
-        You need to share information about your team members, their education, experience, and other details such as location and contact information.
-
-        There are many different ways to design an about us page for your business or personal site. Some people prefer a minimalistic style, and some like more complex designs with animations and videos.
-
-        The main idea is to create a unique design that will match your brand image and give an interesting experience to users who visit your site.
+     <p>Hello</p>
     </p>
   </div>
   {/* <div className="container">
